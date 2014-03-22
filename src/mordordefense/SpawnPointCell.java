6ae48aedd838d29fee92e3bf611423636db21da5
brabulla <bulla.adam@gmail.com>
2014-03-22 14:14:02 +0100

    SpawnPointCell konstruktor

--- src/mordordefense/SpawnPointCell.java
+++ src/mordordefense/SpawnPointCell.java
@@ -1,6 +1,13 @@
 package mordordefense;
 
+import mordordefense.testing.Logging;
+
 public class SpawnPointCell extends RouteCell {
+	
+	public SpawnPointCell(int x, int y){
+		super(x,y);
+		Logging.log(">>SpawnPointCell konstruktor hívása, x: "+x+" y: "+y);
+	}
 	
 	@Override
 	public String getType() {
