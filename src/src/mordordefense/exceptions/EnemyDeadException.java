97d54a5cb4c76a7c28ddb9cb61fcf215a7d8511f
bdorca <bdorca@gmail.con>
2014-03-23 19:19:33 +0100

    és most

--- src/src/mordordefense/exceptions/EnemyDeadException.java
+++
@@ -1,8 +0,0 @@
-package mordordefense.exceptions;
-
-public class EnemyDeadException extends Exception {
-
-	// Hogy ne sirjon az eclipse
-	private static final long serialVersionUID = 83407821192490239L;
-
-}
