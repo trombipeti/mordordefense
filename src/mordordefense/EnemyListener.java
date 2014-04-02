7fc2c9ccf5bee299303834ee2314eee234d2764d
Trombitás Péter <trombipeti@gmail.com>
2014-04-02 08:55:04 +0200

    Script interpreter errorok javítva, Trap és Tower default konstruktor, Controller setRandom meg setMapFileName.

---
+++ src/mordordefense/EnemyListener.java
@@ -0,0 +1,23 @@
+package mordordefense;
+
+/**
+ * Az enemykkel történő eseményekről értesítő interfész.
+ * 
+ */
+public interface EnemyListener {
+	/**
+	 * Kettévágós típusú lövedék esetén létrejött új enemyről értesítő függvény.
+	 * 
+	 * @param e
+	 *            A kettévágás során létrejött új {@link Enemy}
+	 */
+	public void onSlice(Enemy e);
+	
+	/**
+	 * Egy enemy meghalásakor meghívódó függvény.
+	 * 
+	 * @param e Az elhunyt hős ellenség.
+	 */
+	public void onDie(Enemy e);
+	
+}
