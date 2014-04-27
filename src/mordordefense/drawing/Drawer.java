1f92e99dc3264b9c041d0048bb45805b6abcf40d
brabulla <bulla.adam@gmail.com>
2014-04-27 19:51:21 +0200

    DrawPanel implementálások, és deklarálások

--- src/mordordefense/drawing/Drawer.java
+++ src/mordordefense/drawing/Drawer.java
@@ -3,14 +3,25 @@ package mordordefense.drawing;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import mordordefense.Cell;
+import mordordefense.Enemy;
+
 public class Drawer {
-	public void drawTower(Graphics g, int x, int y){
+	public void drawTower(Graphics g, int x, int y) {
 		g.setColor(Color.YELLOW);
 		g.fillOval(x, y, 40, 40);
 	}
-	
-	public void drawTrap(Graphics g, int x, int y){
+
+	public void drawTrap(Graphics g, int x, int y) {
 		g.setColor(Color.gray);
 		g.fillRect(x, y, 40, 40);
+	}
+
+	public void drawEnemy(Graphics g, int x, int y, Enemy e) {
+		// TODO Implementálni
+	}
+
+	public void drawCell(Graphics g, int x, int y, Cell c) {
+		// TODO Implementálni
 	}
 }
