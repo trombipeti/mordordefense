33ec75240993c52fad635c4beb90daf4a20b60d4
bdorca <bdorca@gmail.con>
2014-04-27 16:59:31 +0200

    wtf

--- src/mordordefense/drawing/DrawPanel.java
+++ src/mordordefense/drawing/DrawPanel.java
@@ -5,21 +5,20 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class DrawPanel extends JPanel
-{
+public class DrawPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private Drawer drawer;
-	public DrawPanel(Drawer d)                       
-	{
+
+	public DrawPanel(Drawer d) {
 		super();
 		setBackground(Color.GREEN);
-		drawer=d;
+		drawer = d;
 	}
 
-	public void paintComponent(Graphics g)  // kirajzolo fuggveny
+	public void paintComponent(Graphics g) // kirajzolo fuggveny
 	{
-		super.paintComponent(g);            //helyes kirajzolas erdekeben
-//		drawer.drawTower(g, 100, 100);
-//		drawer.drawTrap(g, 0, 0);
+		super.paintComponent(g); // helyes kirajzolas erdekeben
+		drawer.drawTower(g, 100, 100);
+		drawer.drawTrap(g, 0, 0);
 	}
 }
--- src/mordordefense/drawing/Drawer.java
+++ src/mordordefense/drawing/Drawer.java
@@ -4,12 +4,12 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Drawer {
-	public void drawTower(Graphics g, int x, int y){
+	public void drawTower(Graphics g, int x, int y) {
 		g.setColor(Color.YELLOW);
 		g.fillOval(x, y, 40, 40);
 	}
-	
-	public void drawTrap(Graphics g, int x, int y){
+
+	public void drawTrap(Graphics g, int x, int y) {
 		g.setColor(Color.gray);
 		g.fillRect(x, y, 40, 40);
 	}
